f239edad56c9f425a6ef9e9ef7af42389e995e97
MrLomaster <timofeikalinin123@gmail.com>
2022-08-02 00:20:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+string[] a = new string[] { "не 1", "не 2", "это 3", "а это 4", "это не 5"};
+
+for(int i= 0; i<a.Length; i++)
+    if (Regex.IsMatch(a[i].ToLower(), "не"))
+    {
+        Console.WriteLine(a[i]);
+    }
+    else
+    {}
